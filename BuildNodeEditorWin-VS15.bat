4a5af2589dcbe95db759f48dfc9a105e7c473c65
zhangdoa <zhangandhang@gmail.com>
2020-03-24 18:13:56 +0800

    Use relative path in the environment variables to invoke msbuild.exe.

--- BuildNodeEditorWin-VS15.bat
+++ BuildNodeEditorWin-VS15.bat
@@ -2,7 +2,7 @@ cd GitSubmodules/imgui-node-editor
 mkdir Build
 cd Build
 cmake -G "Visual Studio 15 Win64" ../
-msbuild ImGuiNodeEditor.sln
+"%VS2017INSTALLDIR%\MSBuild\15.0\Bin\msbuild.exe" ImGuiNodeEditor.sln
 xcopy /s/e/y NodeEditor\Debug\*.lib ..\..\..\Build\LibArchive\Debug\
 xcopy /s/e/y ThirdParty\imgui\Debug\*.lib ..\..\..\Build\LibArchive\Debug\
 pause
--- BuildWin-VS15.bat
+++ BuildWin-VS15.bat
@@ -2,5 +2,5 @@ mkdir Build
 cd Build
 cmake -DCMAKE_BUILD_TYPE=DEBUG -G "Visual Studio 15 Win64" ../source
 cmake -DCMAKE_BUILD_TYPE=DEBUG -G "Visual Studio 15 Win64" ../source
-msbuild Waveless.sln
+"%VS2017INSTALLDIR%\MSBuild\15.0\Bin\msbuild.exe" Waveless.sln
 pause
