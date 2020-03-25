f570b89128e7c78ca065b2c24517deccdb8f778a
zhangdoa <zhangandhang@gmail.com>
2020-03-26 00:47:21 +0800

    Change the CMake config file path to uppercase.

--- BuildWin-VS15.bat
+++ BuildWin-VS15.bat
@@ -1,6 +1,6 @@
 mkdir Build
 cd Build
-cmake -DCMAKE_BUILD_TYPE=DEBUG -G "Visual Studio 15 Win64" ../source
-cmake -DCMAKE_BUILD_TYPE=DEBUG -G "Visual Studio 15 Win64" ../source
+cmake -DCMAKE_BUILD_TYPE=DEBUG -G "Visual Studio 15 Win64" ../Source
+cmake -DCMAKE_BUILD_TYPE=DEBUG -G "Visual Studio 15 Win64" ../Source
 "%VS2017INSTALLDIR%\MSBuild\15.0\Bin\msbuild.exe" Waveless.sln
 pause
